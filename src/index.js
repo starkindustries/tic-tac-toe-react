91b5fbe8a7b4ec35e4c1f5129cca016af1afb530
Zion Perez <zionperez@gmail.com>
2018-11-05 17:21:51 +0900

    Completed 'lifting up state' section.

--- src/index.js
+++ src/index.js
@@ -5,16 +5,46 @@ import './index.css';
 class Square extends React.Component {
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button 
+            className="square" 
+            onClick={ () =>  this.props.onClick()}>
+            {this.props.value}
         </button>
       );
     }
-  }
-  
+  }    
+
+/***************************
+ * Note
+ * The DOM <button> element’s onClick attribute has a special meaning to React because
+ * it is a built-in component. For custom components like Square, the naming is up to 
+ * you. We could name the Square’s onClick prop or Board’s handleClick method differently. 
+ * In React, however, it is a convention to use on[Event] names for props which represent 
+ * events and handle[Event] for the methods which handle the events.
+ ***************************/
+
+
   class Board extends React.Component {
+    constructor(props){ 
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
     renderSquare(i) {
-      return <Square />;
+        return (
+            <Square 
+                value={this.state.squares[i]} 
+                onClick= { () => this.handleClick(i) }    
+            />
+        );
     }
   
     render() {
